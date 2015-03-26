e0c2ff871ce336ff1707df44857ac34896336380
Ben Ramey <ben.ramey@gmail.com>
2015-03-26 13:34:24 -0500

    Persistence and CombGuid tests

---
+++ src/UnitTests/EntityFrameworkPersistenceTests.cs
@@ -0,0 +1,44 @@
+﻿#region Usings
+
+using System;
+using System.Linq;
+using FluentAssertions;
+using GoodlyFere.NServiceBus.EntityFramework;
+using NServiceBus.Persistence;
+using Xunit;
+
+#endregion
+
+namespace UnitTests
+{
+    public class EntityFrameworkPersistenceTests
+    {
+        [Fact]
+        public void SupportsTimeouts()
+        {
+            var obj = new EntityFrameworkPersistence();
+            obj.HasSupportFor<StorageType.Timeouts>().Should().BeTrue();
+        }
+
+        [Fact]
+        public void SupportsSubscriptions()
+        {
+            var obj = new EntityFrameworkPersistence();
+            obj.HasSupportFor<StorageType.Subscriptions>().Should().BeTrue();
+        }
+
+        [Fact]
+        public void SupportsSagas()
+        {
+            var obj = new EntityFrameworkPersistence();
+            obj.HasSupportFor<StorageType.Sagas>().Should().BeTrue();
+        }
+
+        [Fact]
+        public void DoesNotSupportOutbox()
+        {
+            var obj = new EntityFrameworkPersistence();
+            obj.HasSupportFor<StorageType.Outbox>().Should().BeFalse();
+        }
+    }
+}
---
+++ src/UnitTests/Support/CombGuidTests.cs
@@ -0,0 +1,27 @@
+﻿#region Usings
+
+using System;
+using System.Linq;
+using FluentAssertions;
+using GoodlyFere.NServiceBus.EntityFramework.Support;
+using Xunit;
+
+#endregion
+
+namespace UnitTests.Support
+{
+    public class CombGuidTests
+    {
+        [Fact]
+        public void NewGuid_ProducesSequentialGuids()
+        {
+            var guid1 = CombGuid.NewGuid().ToByteArray();
+            var guid2 = CombGuid.NewGuid().ToByteArray();
+
+            var guid1Int = BitConverter.ToInt32(guid1, guid1.Length - 4);
+            var guid2Int = BitConverter.ToInt32(guid2, guid2.Length - 4);
+
+            guid1Int.Should().BeLessOrEqualTo(guid2Int);
+        }
+    }
+}
